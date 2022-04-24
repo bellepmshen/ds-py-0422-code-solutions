9a4d93d6db600c87c978dfe2c0926569bebf0644
bellepmshen <bellepeiminshen@gmail.com>
2022-04-24 00:05:24 +0000

    Completed ds-prep-example exercise.

---
+++ __sample__/example.py
@@ -0,0 +1,9 @@
+print("\n================")
+print("ds-prep-example")
+print("================\n")
+
+
+
+print("I would like to analyze\n")
+print("stellar spectroscopy\n")
+print("to learn the stellar evolution model.")
